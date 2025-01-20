89c9ad45d1956123a580c4902c357b0ebc82cf1d
bd-designer-rokib <rokibulhassan1234@gmail.com>
2025-01-20 15:30:34 +0600

    All Section Design Complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["*"],
+  content: ["./**.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        manrope: ["Manrope", "serif"],
+        jakarta: ["Plus Jakarta Sans", "serif"],
+      },
+      colors: {
+        shade_1: "#1C1C1C",
+        shade_2: "#777",
+        shade_3: "#5767391A",
+        shade_4: "#E6A6231A",
+        shade_5: "#F4F4F4",
+        shade_6: "#5D5A6F",
+        shade_7: "#D2D2D2",
+        shade_8: "#494949",
+      },
+      boxShadow: {
+        shadow_1: "0px 7px 50px 0px rgba(0,0,0,0.06)",
+      },
+      backgroundImage: {
+        "card-image": "url('/assets/images/client.png')",
+      },
+    },
   },
-  plugins: [],
-}
+  plugins: [require("daisyui")],
+};
